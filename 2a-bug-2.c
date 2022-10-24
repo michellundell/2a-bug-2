2ad689b00d3126cedae49649f4a49cf11a55acd1
michellundell <michel@lundell.net>
2022-10-24 05:33:32 +0200

    initial commit

---
+++ 2a-bug-2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char **argv)
+{
+	char *sP = malloc(10);
+	strcpy(sP,"Haj pau deg, horret idau?");
+
+	/* 
+	** loop through memory and print the chars until end of string
+	** sP++ moves the pointer one step from current position.
+	*/
+
+	while( *sP++ != '\0' ) {
+		printf("%c",*sP);
+	} 
+	printf("\n");
+
+	return 0;
+}
